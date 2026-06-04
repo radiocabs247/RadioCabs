a09af5967421699169683c78953f50d4c29f9fc0
radiocabs247 <radiocabs247@gmail.com>
2026-06-04 09:09:18 -0500

    Add files via upload

---
+++ Views/Admin/AdminPayments.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Radiocab.Views.Admin
+{
+    public class AdminPaymentsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
